9a3fc2991fc6852cd249c43ce89107a28d9f5cae
Diogo da Silva Fernandes <diogosilvafernandes85@gmail.com>
2024-08-21 11:42:42 +0200

    feat: Add table style and information show

--- index.php
+++ index.php
@@ -17,47 +17,78 @@
         td,
         th{
             padding: 5px;
+            text-align: center;
         }
 
         table{
             border-collapse: collapse;
+            width: 100%;
         }
 
         caption{
             font-weight: bold;
             padding: 10px 0;
         }
+
+        form{
+            width: 30vw;
+        }
+
+        input[type='submit']{
+            margin-top: 5px;
+            width: 100%;
+            padding: 5px 0;
+        }
     </style>
 </head>
 <body>
-    <table>
-        <caption>Tableau des skills</caption>
+    <form action="/update.php" method="post">
+        <table>
+            <caption>Tableau des skills</caption>
 
-        <tr>
-            <th></th>
-            <th>HTML</th>
-            <th>PHP (base)</th>
-            <th>PHP (avec OO)</th>
-        </tr>
-        <?php
-            $file = 'skills.csv';
-            $stream = fopen($file, 'r');
+            <tr>
+                <th></th>
+                <th>HTML</th>
+                <th>PHP (base)</th>
+                <th>PHP (avec OO)</th>
+            </tr>
+            <?php
+                $file = 'skills.csv';
+                $stream = fopen($file, 'r');
 
-            if ($stream != FALSE)
-            {
-                while (($row = fgetcsv($stream, null, ';')) != FALSE)
+                if ($stream != FALSE)
                 {
-                    ?> <tr> <?php
-        
-                    foreach ($row as $value)
-                    {
-                        ?> <td><?=$value?></td> <?php
-                    }
+                    $rowNumber = 1;
 
-                    ?> </tr> <?php
+                    while (($row = fgetcsv($stream, null, ';')) != FALSE)
+                    {
+                        ?> <tr> <?php
+
+                        foreach ($row as $col => $value)
+                        {
+                            if ($col == 0)
+                            {
+                                ?>
+                                <td>
+                                    <?=$value?>
+                                    <input type="hidden" name="name-<?=$rowNumber?>" value="<?=$value?>">
+                                </td>
+                                <?php
+                            }
+                            else
+                            {
+                                ?> <td><input type="number" name="skill-<?=$rowNumber?>-<?=$col?>" id="skill" min="1" max="4" value="<?=$value?>"></td> <?php
+                            }
+                        }
+
+                        ?> </tr> <?php
+
+                        $rowNumber++;   
+                    }
                 }
-            }
-        ?>
-    </table>
+            ?>
+        </table>
+        <input type="submit" value="Sauvegarder">
+    </form>
 </body>
 </html>
